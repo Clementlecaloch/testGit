b1f2442f6911872e5976d16911f92fdc29476b2e
Clement Le Caloch <clement.le-caloch@etu.unilim.fr>
2019-04-02 16:33:56 +0200

    Jouons ensemble

--- src/main/java/fr/unilim/iut/testGit/HelloWorld.java
+++ src/main/java/fr/unilim/iut/testGit/HelloWorld.java
@@ -3,5 +3,6 @@ package fr.unilim.iut.testGit;
 public class HelloWorld {
 	public static void main (String args[]) {
 		System.out.println("hello world!");
+		System.out.println( "Jouons ensemble!" );
 	}
 }
